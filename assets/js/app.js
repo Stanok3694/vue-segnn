fb293d5302d170574c7c448c3aa647d55eeccc89
Stanislav Okunev <stanok3694@yandex.ru>
2017-04-18 16:42:19 +0300

    Add one vue-component into project
    Tried to implement vue-components into the project but it was not the best idea. Think about different ways.

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var Footer = {
+    template: '<footer><p class="pull-right"><a href="#">Наверх</a></p><p>&copy; 2017 СтройЭлектроГрупп &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p></footer>'
+}
+
+new Vue({
+    el: "#app",
+    data: {},
+    methods: {},
+    components:{
+        'my-footer': Footer
+    }
+})

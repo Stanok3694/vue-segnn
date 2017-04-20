370a625e6b3a7a8be1d9cbfd49c29bca0511c1c4
Stanislav Okunev <stanok3694@yandex.ru>
2017-04-20 17:23:38 +0300

    Add feature component + found data issue -> should investigate and fix
    data issue in some set methods -> reverse enginering and fix that

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,72 @@ Vue.component('my-footer', {
         }
     }
 })
+// feature-content-component
+Vue.component('feature-content', {
+    props: ['type'],
+    template: '<div :class="setFeatureContainerClass()"><h2 :class="setFeatureHeaderTwoClass()">{{featureTitle}}</h2><p :class="setParagraphClass()">{{featureContent}}</p></div>',
+    data: function(){
+        return {
+            featureTitle: this.setFeatureHeader(),
+            featureClasses: {
+                divClass: 'col-md-7',
+                headerTwoClass: 'featurette-heading',
+                paragraphClass: 'lead'
+            },
+            featureHeaders:{
+                firstHeader: 'Качество',
+                secondHeader: 'Опыт',
+                thirdHeader: 'Гибкость',
+            },
+            featureContent: this.setFeatureContent()            
+        }
+    },
+    methods: {
+        setFeatureHeader: function(){
+            if(this.type == '1'){
+                return featureHeaders.firstHeader;
+            }
+            if(this.type == '2'){
+                return featureHeaders.secondHeader;
+            }
+            if(this.type == '3'){
+                return featureHeaders.thirdHeader;
+            }
+        },
+        setFeatureContent: function(){
+            if(this.type == '1'){
+                return 'Donec ullamcorper nulla non metus auctor fringilla.' 
+                     + 'Vestibulum id ligula porta felis euismod semper. ' 
+                     + 'Praesent commodo cursus magna, vel scelerisque nisl consectetur. '
+                     + 'Fusce dapibus, tellus ac cursus commodo.';
+            }
+            if(this.type == '2'){
+                return 'Bonec ullamcorper nulla non metus auctor fringilla.' 
+                     + 'Vestibulum id ligula porta felis euismod semper. ' 
+                     + 'Praesent commodo cursus magna, vel scelerisque nisl consectetur. '
+                     + 'Fusce dapibus, tellus ac cursus commodo.';
+            }
+            if(this.type == '3'){
+                return 'Lonec ullamcorper nulla non metus auctor fringilla.' 
+                     + 'Vestibulum id ligula porta felis euismod semper. ' 
+                     + 'Praesent commodo cursus magna, vel scelerisque nisl consectetur. '
+                     + 'Fusce dapibus, tellus ac cursus commodo.';
+            }
+            return 'Sorry, there is no data!'
+        },
+        setFeatureContainerClass: function(){
+            if(this.type == '1') return this.featureClasses.divClass
+            if(this.type == '2') return this.featureClasses.divClass + 'col-md-push-5'
+            if(this.type == '3') return this.featureClasses.divClass
+        },
+        setFeatureHeaderTwoClass: function(){
+            return this.featureClasses.headerTwoClass
+        },
+        setParagraphClass: function(){
+            return this.featureClasses.paragraphClass
+        }
+    }
+})
 
 new Vue({
     el: "#app",
